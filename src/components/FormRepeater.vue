2c9ff3b463841722735a3fc72a4bed2c14ed9979
Sam Macharia <sam.macharia@gosuperscript.com>
2023-06-14 08:57:12 +0100

    feat: recommended design from backend

--- src/components/FormRepeater.vue
+++ src/components/FormRepeater.vue
@@ -6,10 +6,26 @@ import FormSelect from './FormSelect.vue';
 const inputFieldMap = {
   input: FormInput,
   select: FormSelect
+} 
+
+const data = {
+  'some-table-control-question-id': [
+    {
+      question1: 'answer1',
+    },
+    {
+      question2: 'answer2',
+    }
+  ]
 }
+
+// const stuff = [{questiondid: "sjdfdsjkfjksf", question2: "value",....}, {questiondid: "sjdfdsjkfjksf", question2: "value",....}]
 
 const backendComponents = ref(
   [
+    { 
+      components: [{ component: 'input' , value: '', name: 'name'}, { component: 'select', value: '', name: 'vehicle'}]
+    },
     { 
       components: [{ component: 'input' , value: '', name: 'name'}, { component: 'select', value: '', name: 'vehicle'}]
     }
